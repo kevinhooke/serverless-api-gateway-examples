bc4d74f08c0720e96a5a87d33f1e64f8748a3e17
kevinhooke <kevin.hooke@gmail.com>
2021-10-23 21:14:51 -0700

    fixed lambda proxy response

--- lambda-apis/index.js
+++ lambda-apis/index.js
@@ -5,5 +5,10 @@ exports.handler =  async function(event, context) {
     console.log("example1 called")
     console.log("EVENT: \n" + JSON.stringify(event, null, 2))
     console.log(JSON.stringify(result));
-    return context.logStreamName
+    return {
+      "isBase64Encoded": false,
+      "statusCode": 200,
+      "headers": {},
+      "body": JSON.stringify(result)
+    }
   }
